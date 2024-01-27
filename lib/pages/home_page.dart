7c8c4644c0674ac0e66b9276f948ae294f56448c
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-27 05:37:46 +0100

    fetching data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
     required this.username,
@@ -9,24 +9,50 @@ class HomePage extends StatelessWidget {
   final String username;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(username),
+        title: Text(widget.username),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return Container(
-            padding: const EdgeInsets.all(9),
-            child: ListTile(
-              title: Text('God', style: TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.bold),),
-              subtitle: Text('active'),
-              trailing: Text('is God'),
-            ),
-          );
+      body: FutureBuilder<List<dynamic>>(
+        future: dataFuture,
+        builder: (context, snapshot){
+          if (snapshot.connectionState == ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError){
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty){
+            return Center(
+              child: Text('No data available'),
+            );
+          } else {
+            List<dynamic> dataList = snapshot.data!;
+            return ListView.builder(
+              itemCount: ,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.all(9),
+                  child: ListTile(
+                    title: Text('God', style: TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.bold),),
+                    subtitle: Text('active'),
+                    trailing: Text('is God'),
+                  ),
+                );
+              },
+            );
+          }
         },
+
       ),
     );
   }
